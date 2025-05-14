300cae57d068fd016c3114242ec3a09915d2b42d
Maynor de Leon <maynoralex@gmail.com>
2025-05-14 14:29:03 +0200

    first commit

---
+++ GraphQLProject/Program.cs
@@ -0,0 +1,32 @@
+using GraphiQl;
+using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Query;
+using GraphQLProject.Schema;
+using GraphQLProject.Services;
+using GraphQLProject.Type;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddTransient<IMenuRepository, MenuRepository>();
+builder.Services.AddTransient<MenuType>();
+builder.Services.AddTransient<MenuQuery>();
+builder.Services.AddTransient<ISchema, MenuSchema>();
+
+builder.Services.AddGraphQL( options => options.AddAutoSchema<ISchema>().AddSystemTextJson() );
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseGraphiQl("/graphql");
+app.UseGraphQL<ISchema>();
+
+app.Run();
+
+
