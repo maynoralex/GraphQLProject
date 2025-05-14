dc289ca36cc9f3b1d1b662c27268ba2dc09cb7d4
Maynor de Leon <maynoralex@gmail.com>
2025-05-14 18:06:40 +0200

    Adding Mutations

--- GraphQLProject/Program.cs
+++ GraphQLProject/Program.cs
@@ -2,6 +2,7 @@ using GraphiQl;
 using GraphQL;
 using GraphQL.Types;
 using GraphQLProject.Interfaces;
+using GraphQLProject.Mutation;
 using GraphQLProject.Query;
 using GraphQLProject.Schema;
 using GraphQLProject.Services;
@@ -15,6 +16,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddTransient<IMenuRepository, MenuRepository>();
 builder.Services.AddTransient<MenuType>();
 builder.Services.AddTransient<MenuQuery>();
+builder.Services.AddTransient<MenuMutation>();
+builder.Services.AddTransient<MenuInputType>();
 builder.Services.AddTransient<ISchema, MenuSchema>();
 
 builder.Services.AddGraphQL( options => options.AddAutoSchema<ISchema>().AddSystemTextJson() );
---
+++ GraphQLProject/Type/MenuInputType.cs
@@ -0,0 +1,14 @@
+using GraphQL.Types;
+
+namespace GraphQLProject.Type;
+
+public class MenuInputType : InputObjectGraphType
+{
+    public MenuInputType()
+    {
+        Field<IntGraphType>("id");
+        Field<StringGraphType>("name");
+        Field<StringGraphType>("description");
+        Field<FloatGraphType>("price");
+    }
+}
